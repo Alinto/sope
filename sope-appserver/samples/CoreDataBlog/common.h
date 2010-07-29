97280a38bd1583e282a90c5513a15c88add3dcca
Ludovic Marcotte <lmarcotte@inverse.ca>
2010-07-29 18:40:21 +0000

    Cleaned up more stuff.
    Monotone-Parent: 592f1af929791fbdc8780afe81d8021d7c6fdd73
    Monotone-Revision: 843d1025bab5d142155d7ded15c9de17d3b599e6
    
    Monotone-Author: ludovic@Sophos.ca
    Monotone-Date: 2010-07-29T18:40:21
    Monotone-Branch: ca.inverse.sope

--- sope-appserver/samples/CoreDataBlog/common.h
+++
@@ -1,5 +0,0 @@
-
-#import <Foundation/Foundation.h>
-#include <NGObjWeb/NGObjWeb.h>
-
-#import <CoreData/CoreData.h>
--- sope-appserver/samples/HelloWorld/common.h
+++
@@ -1,3 +0,0 @@
-
-#import <Foundation/Foundation.h>
-#include <NGObjWeb/NGObjWeb.h>
