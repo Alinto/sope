6c910bc7b5c2150b2315be6b2320a878f0014144
Ludovic Marcotte <lmarcotte@inverse.ca>
2015-01-23 16:13:07 -0500

    More cleanups

--- sope-gdl1/PostgreSQL/PostgreSQL72Channel.m
+++ sope-gdl1/PostgreSQL/PostgreSQL72Channel.m
@@ -38,6 +38,8 @@
 
 #include "pgconfig.h"
 
+#import <NGExtensions/NSObject+Logs.h>
+
 #ifndef MIN
 #  define MIN(x, y) ((x > y) ? y : x)
 #endif
@@ -117,7 +119,7 @@ static Class    MDictClass  = Nil;
 }
 
 - (void)receivedMessage:(NSString *)_message {
-  NSLog(@"%@: message: %@", self, _message);
+  [self logWithFormat: @"message: %@", _message];
 }
 
 static void _pgMessageProcessor(void *_channel, const char *_msg)
