2d62934fcc2d66192b6c7c2b7de854af0360fe2d
Euan Thoms <euan@potensol.com>
2015-10-22 16:55:11 +0800

    Stage 4 of clang compiler warning patches. Various code cleanup and fixes.

--- sope-appserver/NGObjWeb/WOMailDelivery.m
+++ sope-appserver/NGObjWeb/WOMailDelivery.m
@@ -180,7 +180,8 @@ WOMailDelivery *sharedInstance = nil;
       NSData *body;
       
       body = [(NSDictionary *)_email objectForKey:@"body"];
-      if (fwrite([body bytes], [body length], 1, toMail) < 0)
+      NSUInteger bytes = [body length];
+      if (fwrite([body bytes], bytes, 1, toMail) < bytes)
         goto failed;
     }
     fprintf(toMail, "\r\n");
--- sope-appserver/NGObjWeb/WebDAV/SoObjectDataSource.m
+++ sope-appserver/NGObjWeb/WebDAV/SoObjectDataSource.m
@@ -137,7 +137,7 @@ static BOOL debugOn = NO;
   pool = [[NSAutoreleasePool alloc] init];
   i=0;
   while ((childKey = [[childKeys nextObject] stringValue]) != nil) {
-    NSDictionary *rec;
+    id rec;
     NSException  *e;
     NSString     *childHref;
     id child = nil;
