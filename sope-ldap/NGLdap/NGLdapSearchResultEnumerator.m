2d38fbe94b515ddf22aad3dca49f5e542188b749
Ludovic Marcotte <lmarcotte@inverse.ca>
2018-03-19 13:42:38 -0400

    (fix) improve LDAP logging

--- sope-ldap/NGLdap/NGLdapSearchResultEnumerator.m
+++ sope-ldap/NGLdap/NGLdapSearchResultEnumerator.m
@@ -185,16 +185,13 @@
   
   res = ldap_result(self->handle, self->msgid, 0, top, &msg);
 
-  if (res == -1)
+  if (res <= 0 || msg == NULL)
     {
       int err;
       ldap_get_option(self->handle, LDAP_OPT_RESULT_CODE, &err);
       NSLog(@"Fatal LDAP error during ldap_result: %s", ldap_err2string(err));
       return nil;
     }
-
-  if (msg == NULL)
-    return nil;
   
   switch(res) {
 #if defined(LDAP_RES_SEARCH_REFERENCE)
