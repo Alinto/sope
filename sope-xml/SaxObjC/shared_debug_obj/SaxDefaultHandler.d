372d3dda9a7c431452a16522f9499faa99677407
Hivert Quentin <quentin.hivert.fr@gmail.com>
2026-02-12 10:03:48 +0100

    test

--- sope-xml/SaxObjC/shared_debug_obj/SaxDefaultHandler.d
+++ sope-xml/SaxObjC/shared_debug_obj/SaxDefaultHandler.d
@@ -1,42 +1,42 @@
 obj/SaxDefaultHandler.o: SaxDefaultHandler.m SaxDefaultHandler.h \
-  /usr/include/GNUstep/Foundation/NSObject.h \
-  /usr/include/GNUstep/Foundation/NSObjCRuntime.h \
-  /usr/include/GNUstep/GNUstepBase/GSVersionMacros.h \
-  /usr/include/GNUstep/GNUstepBase/preface.h \
-  /usr/include/GNUstep/GNUstepBase/objc-gnu2next.h \
-  /usr/include/GNUstep/GNUstepBase/GSConfig.h \
-  /usr/include/GNUstep/GNUstepBase/GSObjCRuntime.h \
-  /usr/include/GNUstep/Foundation/NSZone.h \
-  /usr/include/GNUstep/GNUstepBase/GNUstep.h \
-  /usr/include/GNUstep/Foundation/NSDate.h ../SaxObjC/SaxEntityResolver.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSObject.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSObjCRuntime.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GSVersionMacros.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/preface.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/objc-gnu2next.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GSConfig.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GSObjCRuntime.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSZone.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GNUstep.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSDate.h ../SaxObjC/SaxEntityResolver.h \
   ../SaxObjC/SaxDTDHandler.h ../SaxObjC/SaxContentHandler.h \
-  /usr/include/GNUstep/Foundation/NSString.h \
-  /usr/include/GNUstep/Foundation/NSRange.h ../SaxObjC/SaxAttributes.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSString.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSRange.h ../SaxObjC/SaxAttributes.h \
   ../SaxObjC/SaxAttributeList.h ../SaxObjC/SaxLocator.h \
   ../SaxObjC/SaxErrorHandler.h SaxException.h \
-  /usr/include/GNUstep/Foundation/NSException.h
+  //usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSException.h
 
 SaxDefaultHandler.h:
 
-/usr/include/GNUstep/Foundation/NSObject.h:
+//usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSObject.h:
 
-/usr/include/GNUstep/Foundation/NSObjCRuntime.h:
+//usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSObjCRuntime.h:
 
-/usr/include/GNUstep/GNUstepBase/GSVersionMacros.h:
+//usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GSVersionMacros.h:
 
-/usr/include/GNUstep/GNUstepBase/preface.h:
+//usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/preface.h:
 
-/usr/include/GNUstep/GNUstepBase/objc-gnu2next.h:
+//usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/objc-gnu2next.h:
 
-/usr/include/GNUstep/GNUstepBase/GSConfig.h:
+//usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GSConfig.h:
 
-/usr/include/GNUstep/GNUstepBase/GSObjCRuntime.h:
+//usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GSObjCRuntime.h:
 
-/usr/include/GNUstep/Foundation/NSZone.h:
+//usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSZone.h:
 
-/usr/include/GNUstep/GNUstepBase/GNUstep.h:
+//usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GNUstep.h:
 
-/usr/include/GNUstep/Foundation/NSDate.h:
+//usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSDate.h:
 
 ../SaxObjC/SaxEntityResolver.h:
 
@@ -44,9 +44,9 @@ SaxDefaultHandler.h:
 
 ../SaxObjC/SaxContentHandler.h:
 
-/usr/include/GNUstep/Foundation/NSString.h:
+//usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSString.h:
 
-/usr/include/GNUstep/Foundation/NSRange.h:
+//usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSRange.h:
 
 ../SaxObjC/SaxAttributes.h:
 
@@ -58,4 +58,4 @@ SaxDefaultHandler.h:
 
 SaxException.h:
 
-/usr/include/GNUstep/Foundation/NSException.h:
+//usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSException.h:
--- sope-xml/SaxObjC/shared_debug_obj/SaxException.d
+++ sope-xml/SaxObjC/shared_debug_obj/SaxException.d
@@ -1,42 +1,42 @@
 obj/SaxException.o: SaxException.m SaxException.h \
-  /usr/include/GNUstep/Foundation/NSException.h \
-  /usr/include/GNUstep/GNUstepBase/GSVersionMacros.h \
-  /usr/include/GNUstep/Foundation/NSString.h \
-  /usr/include/GNUstep/Foundation/NSObject.h \
-  /usr/include/GNUstep/Foundation/NSObjCRuntime.h \
-  /usr/include/GNUstep/GNUstepBase/preface.h \
-  /usr/include/GNUstep/GNUstepBase/objc-gnu2next.h \
-  /usr/include/GNUstep/GNUstepBase/GSConfig.h \
-  /usr/include/GNUstep/GNUstepBase/GSObjCRuntime.h \
-  /usr/include/GNUstep/Foundation/NSZone.h \
-  /usr/include/GNUstep/GNUstepBase/GNUstep.h \
-  /usr/include/GNUstep/Foundation/NSDate.h \
-  /usr/include/GNUstep/Foundation/NSRange.h
+  //usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSException.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GSVersionMacros.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSString.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSObject.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSObjCRuntime.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/preface.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/objc-gnu2next.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GSConfig.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GSObjCRuntime.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSZone.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GNUstep.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSDate.h \
+  //usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSRange.h
 
 SaxException.h:
 
-/usr/include/GNUstep/Foundation/NSException.h:
+//usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSException.h:
 
-/usr/include/GNUstep/GNUstepBase/GSVersionMacros.h:
+//usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GSVersionMacros.h:
 
-/usr/include/GNUstep/Foundation/NSString.h:
+//usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSString.h:
 
-/usr/include/GNUstep/Foundation/NSObject.h:
+//usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSObject.h:
 
-/usr/include/GNUstep/Foundation/NSObjCRuntime.h:
+//usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSObjCRuntime.h:
 
-/usr/include/GNUstep/GNUstepBase/preface.h:
+//usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/preface.h:
 
-/usr/include/GNUstep/GNUstepBase/objc-gnu2next.h:
+//usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/objc-gnu2next.h:
 
-/usr/include/GNUstep/GNUstepBase/GSConfig.h:
+//usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GSConfig.h:
 
-/usr/include/GNUstep/GNUstepBase/GSObjCRuntime.h:
+//usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GSObjCRuntime.h:
 
-/usr/include/GNUstep/Foundation/NSZone.h:
+//usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSZone.h:
 
-/usr/include/GNUstep/GNUstepBase/GNUstep.h:
+//usr/include/x86_64-linux-gnu/GNUstep/GNUstepBase/GNUstep.h:
 
-/usr/include/GNUstep/Foundation/NSDate.h:
+//usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSDate.h:
 
-/usr/include/GNUstep/Foundation/NSRange.h:
+//usr/include/x86_64-linux-gnu/GNUstep/Foundation/NSRange.h:
